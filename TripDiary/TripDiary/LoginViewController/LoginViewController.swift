6aeb6009dc3ebba7945e2b0a8b73615db6fe4e05
이운형 <>
2021-01-10 01:20:35 +0900

    [Feat] TripDiary project 생성

---
+++ TripDiary/TripDiary/LoginViewController/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  TripDiary
+//
+//  Created by 이운형 on 2021/01/09.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
