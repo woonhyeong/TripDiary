244ba60ded07a4768a32517d1f4018edc56ddf66
이운형 <>
2021-01-10 12:48:20 +0900

    [Feat] 폰트 추가 / MainViewController 폰트 적용 1. 나눔고딕, 나눔스퀘어 폰트 추가 2. MainViewController 폰트 적용 3. NavigationBackButton image resource 추가

---
+++ TripDiary/TripDiary/Main/MainViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MainViewController.swift
+//  TripDiary
+//
+//  Created by 이운형 on 2021/01/09.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    // MARK: - UI Properties
+    @IBOutlet private weak var logInButton: UIButton!
+    @IBOutlet private weak var signUpButton: UIButton!
+    
+    // MARK: - ViewController Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    // MARK: - IBActions
+    @IBAction func logInButtonTouched(_ sender: Any) {
+    }
+    
+    @IBAction func signUpButtonTouched(_ sender: Any) {
+    }
+}
+
+extension MainViewController {
+    private func setupUI() {
+        logInButton.layer.cornerRadius = 14.0
+        signUpButton.layer.cornerRadius = 14.0
+    }
+}
